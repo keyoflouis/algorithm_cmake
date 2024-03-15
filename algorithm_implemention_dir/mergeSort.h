e20bdd3887474e9eccbc3578bb1e3f8e302c034b
keyoflouis <132821554+keyoflouis@users.noreply.github.com>
2024-03-15 10:55:02 +0800

    添加了mergeSort的循环的实现

--- algorithm_implemention_dir/mergeSort.h
+++ algorithm_implemention_dir/mergeSort.h
@@ -1,5 +1,5 @@
 #pragma once
-#include"insertionSort.h"
+#include "insertionSort.h"
 int globle_test = 0;
 
 template <class T>
@@ -42,8 +42,9 @@ template <class T>
 void __mergeSort(T arr[], int l, int r)
 {
     /*if(l>r) return;*/
-    if(r -l <=15){
-        insertionSort(arr,l,r);
+    if (r - l <= 15)
+    {
+        insertionSort(arr, l, r);
         return;
     }
 
@@ -63,4 +64,12 @@ void mergeSort(T arr[], int n)
 {
     // std::cout << "first palce" << std::endl;
     __mergeSort(arr, 0, n - 1);
+}
+
+template <class T>
+void mergeSortBU(T arr[], int n)
+{
+    for (int sz = 1; sz <= n; sz += sz)
+        for (int i = 0; i + sz < n; i += sz + sz)
+            __merge(arr, i, i + sz - 1, min((i + sz) + sz - 1, n - 1));
 }
